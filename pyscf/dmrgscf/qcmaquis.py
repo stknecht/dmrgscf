a633385e1ab0793a5fd4b5294c905bec3d2fa37b
Stefan Knecht <stefan.knecht@phys.chem.ethz.ch>
2022-07-08 10:46:11 +0200

    CASCI with QCMaquis as FCIsolver runs through but does not do any optimization yet. :)

--- pyscf/dmrgscf/qcmaquis.py
+++ pyscf/dmrgscf/qcmaquis.py
@@ -35,6 +35,7 @@ from pyscf import mcscf
 from pyscf.dmrgscf import dmrg_sym
 from pyscf import __config__
 
+ndpointer = numpy.ctypeslib.ndpointer
 
 # Settings
 try:
@@ -68,6 +69,48 @@ sayhello.argtypes = [
     ctypes.c_char_p,
 ]
 
+# qcmINIT = libqcm.qcmaquis_interface_preinit(int nel, int L, int spin, int irrep,
+#                                 const int* site_types, V conv_thresh, int m, int nsweeps,
+#                                 const int* sweep_m, int nsweepm, const char* project_name,
+#                                 bool meas_2rdm)
+
+qcmINIT = libqcm.qcmaquis_interface_preinit
+qcmINIT.restype = None
+qcmINIT.argtypes = [
+    ctypes.c_int32,
+    ctypes.c_int32,
+    ctypes.c_int32,
+    ctypes.c_int32,
+    ndpointer(ctypes.c_int32, flags="C_CONTIGUOUS"),
+    ctypes.c_double,
+    ctypes.c_int32,
+    ctypes.c_int32,
+    ndpointer(ctypes.c_int32, flags="C_CONTIGUOUS"),
+    ctypes.c_int32,
+    ctypes.c_char_p,
+    ctypes.c_bool,
+]
+
+# qcmRDM12 = libqcm.qcmaquis_interface_get_2rdm(int* indices, V* values, int size)
+qcmRDM12 = libqcm.qcmaquis_interface_get_2rdm
+qcmRDM12.restype = None
+qcmRDM12.argtypes = [
+    ndpointer(ctypes.c_int32, flags="C_CONTIGUOUS"),
+    ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
+    ctypes.c_int32,
+]
+
+# qcmRDM12 = libqcm.qcmaquis_interface_update_integrals(const int* integral_indices, const V* integral_values, int integral_size) 
+qcmUPINT = libqcm.qcmaquis_interface_update_integrals
+qcmUPINT.restype = None
+qcmUPINT.argtypes = [
+    ndpointer(ctypes.c_int32, flags="C_CONTIGUOUS"),
+    ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
+    ctypes.c_int32,
+]
+
+
+
 class qcmDMRGCI(lib.StreamObject):
     '''QCMaquis program interface and the object to hold QCMaquis program input parameters.
 
@@ -82,13 +125,13 @@ class qcmDMRGCI(lib.StreamObject):
     >>> mc.kernel()
     -74.379770619390698
     '''
-    def __init__(self, mol=None, maxM=None, tol=None, num_thrds=1, memory=None):
+    def __init__(self, mol=None, maxM=None, tol=None, num_thrds=1,
+                 maxIter=10):
         self.mol = mol
         print("hello my friend")
         string1 = "YEAH!"
         # create byte objects from the strings
-        b_string1 = string1.encode('utf-8')
-        sayhello(b_string1)
+        sayhello(string1.encode('utf-8'))
         if mol is None:
             self.stdout = sys.stdout
             self.verbose = logger.NOTE
@@ -96,29 +139,11 @@ class qcmDMRGCI(lib.StreamObject):
             self.stdout = mol.stdout
             self.verbose = mol.verbose
         self.outputlevel = 2
-        self.hf_occ = 'integral'
 
-
-        # self.executable = settings.BLOCKEXE
-        # self.scratchDirectory = os.path.abspath(settings.BLOCKSCRATCHDIR)
-        # self.mpiprefix = settings.MPIPREFIX
-        self.memory = memory
-
-        self.integralFile = "FCIDUMP"
-        self.configFile = "dmrg.conf"
-        self.outputFile = "dmrg.out"
-        if getattr(settings, 'BLOCKRUNTIMEDIR', None):
-            self.runtimeDir = settings.BLOCKRUNTIMEDIR
-        else:
-            self.runtimeDir = '.'
-        self.maxIter = 20
-        self.approx_maxIter = 6
-        self.twodot_to_onedot = 15
-        self.dmrg_switch_tol = 1e-3
-        self.nroots = 1
+        self.maxIter = 10
+        self.nroots  = 1
         self.weights = []
-        self.wfnsym = 1
-        self.extraline = []
+        self.wfnsym  = 1
 
         if tol is None:
             self.tol = 1e-7
@@ -129,13 +154,7 @@ class qcmDMRGCI(lib.StreamObject):
         else:
             self.maxM = maxM
         self.num_thrds= num_thrds
-        self.startM =  None
         self.restart = False
-        self.nonspinAdapted = False
-        self.scheduleSweeps = []
-        self.scheduleMaxMs  = []
-        self.scheduleTols   = []
-        self.scheduleNoises = []
         self.onlywriteIntegral = False
         self.spin = 0
         self.orbsym = []
@@ -150,33 +169,12 @@ class qcmDMRGCI(lib.StreamObject):
         # don't modify the following attributes, if you do not finish part of calculation, which can be reused.
         #DO NOT CHANGE these parameters, unless you know the code in details
         self.twopdm = True #By default, 2rdm is calculated after the calculations of wave function.
-        self.block_extra_keyword = [] #For Block advanced user only.
-        self.has_fourpdm = False
-        self.has_threepdm = False
-        self.has_nevpt = False
         # This flag _restart is set by the program internally, to control when to make
-        # Block restart calculation.
+        # a QCMaquis restart calculation.
         self._restart = False
-        # self.generate_schedule()
         self.returnInt = False
         self._keys = set(self.__dict__.keys())
 
-
-    @property
-    def max_memory(self):
-        if self.memory is None:
-            return self.memory
-        elif isinstance(self.memory, int):
-            return self.memory * 1e3 # GB -> MB
-        else:  # str
-            val, unit = self.memory.split(',')
-            if unit.trim().upper() == 'G':
-                return float(val) * 1e3
-            else: # MB
-                return float(val)
-    @max_memory.setter
-    def max_memory(self, x):
-        self.memory = x * 1e-3
 
     @property
     def threads(self):
@@ -185,3 +183,105 @@ class qcmDMRGCI(lib.StreamObject):
     def threads(self, x):
         self.num_thrds = x
 
+    def get_size12(norb):
+        sz = 0
+        p = 0
+        while p < norb:
+            q = 0
+            while q < norb:
+                r = 0
+                while r < min(p,q):
+                    s = r
+                    while s < norb:
+                        sz+=1
+                        s+=1
+                    r+=1
+                q+=1
+            p+=1
+        return sz
+
+    def make_rdm1(self, state, norb, nelec, link_index=None, **kwargs):
+        # Avoid calling self.make_rdm12 because it may be overloaded
+        return qcmDMRGCI.make_rdm12(self, state, norb, nelec, link_index, **kwargs)[0]
+
+    def make_rdm12(self, state, norb, nelec, link_index=None, **kwargs):
+        nelectrons = 0
+        if isinstance(nelec, (int, numpy.integer)):
+          nelectrons = nelec
+        else:
+          nelectrons = nelec[0]+nelec[1]
+
+        # The 2RDMs written by "save_spatial_twopdm_text" in BLOCK and STACKBLOCK
+        # are written as E2[i1,j2,k2,l1]
+        # and stored here as E2[i1,l1,j2,k2] (for PySCF purposes)
+        # This is NOT done with SQA in mind.
+        sz = qcmDMRGCI.get_size12(norb)
+        rdm2 = numpy.zeros( (  sz) )
+        ints = numpy.zeros( (4*sz), dtype=numpy.int32 )
+        # qcmRDM12(ints, rdm2, sz)
+
+        twopdm = numpy.zeros( (norb, norb, norb, norb) )
+
+        # (This is coherent with previous statement about indexes)
+        onepdm = numpy.einsum('ikjj->ki', twopdm)
+        onepdm /= (nelectrons-1)
+        return onepdm, twopdm
+
+    def kernel(self, h1e, eri, norb, nelec, fciRestart=None, ecore=0, **kwargs):
+        if self.nroots == 1:
+            roots = 0
+        else:
+            roots = range(self.nroots)
+        if fciRestart is None:
+            fciRestart = self.restart or self._restart
+
+        if 'orbsym' in kwargs:
+            self.orbsym = kwargs['orbsym']
+
+        ''' transfer Hamiltonian (1e- and 2e-integrals) to QCMaquis '''
+        print('hello from kernel - sz is {}'.format(qcmDMRGCI.get_size12(norb = norb)))
+        print('hello from kernel - 1e ints are')
+        print(h1e)
+        print('hello from kernel - 2e ints are')
+        print(eri)
+        print('hello from kernel - ecore is {}'.format(ecore))
+        # setHAM(self, h1e, eri, norb, nelec, ecore)
+        #writeDMRGConfFile(self, nelec, fciRestart)
+        #if self.verbose >= logger.DEBUG1:
+        #    inFile = os.path.join(self.runtimeDir, self.configFile)
+        #    logger.debug1(self, 'Block Input conf')
+        #    logger.debug1(self, open(inFile, 'r').read())
+        if self.onlywriteIntegral:
+            logger.info(self, 'Only write integral')
+            try:
+                calc_e = 0.0 # readEnergy(self)
+            except IOError:
+                if self.nroots == 1:
+                    calc_e = 0.0
+                else :
+                    calc_e = [0.0] * self.nroots
+            return calc_e, roots
+        if self.returnInt:
+            return h1e, eri
+
+        # executeBLOCK(self)
+        # if self.verbose >= logger.DEBUG1:
+        #     outFile = os.path.join(self.runtimeDir, self.outputFile)
+        #     logger.debug1(self, open(outFile).read())
+        # calc_e = readEnergy(self)
+        # if self.restart:
+        #     # Restart only the first iteration
+        #     self.restart = False
+        calc_e = 0.0 # readEnergy(self)
+
+        return calc_e, roots
+
+    def setHAM():
+
+        if isinstance(nelec, (int, numpy.integer)):
+            neleca = nelec//2 + nelec%2
+            nelecb = nelec - neleca
+        else :
+            neleca, nelecb = nelec
+
+        # qcmUPINT()
